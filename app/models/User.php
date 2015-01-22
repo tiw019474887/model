604c68fe58d1a9fe1ba5a309a38c2a73dde65311
tiw019474887 <tiw_t1@hotmail.com>
2015-01-22 17:57:17 +0700

    เพิ่ม

--- app/models/User.php
+++ app/models/User.php
@@ -4,23 +4,46 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Vinelab\NeoEloquent\Eloquent\SoftDeletingTrait;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends NeoEloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, SoftDeletingTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    protected  $label = ['User'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+    protected $fillable = ['email','title','firstname','lastname'];
+
+    protected $hidden = array('password');
+
+    public function getName(){
+        return "$this->title $this->firstname $this->lastname";
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->id;
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function profileImage(){
+        return $this->hasOne('Photo','PROFILEPHOTO');
+    }
+
+    public function news(){
+        return $this->hasMany('News',"CREATENEWS");
+    }
+
+    public static  function isUniqueEmail($email){
+        $user = User::where('email','=',$email)->first();
+        if ($user){
+            return false;
+        }
+        return true;
+    }
 
 }
